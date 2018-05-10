393d56a2208a1ce82a2735379ed486a3b73e36c3
wanghaiyang <wqzbxh@163.com>
2018-05-10 23:51:53 +0800

    优化主页

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -187,31 +187,31 @@ class Index extends Controller
 	public function downRedis(){
            header("Content-type:text/html;charset=utf-8");   
         //$file_path="testMe.txt";  
-        //用以解决中文不能显示出来的问题   
-        //$file_name=iconv("utf-8","gb2312",$file_name);  
-        //$file_sub_path=$_SERVER['DOCUMENT_ROOT']."marcofly/phpstudy/down/down/";   
-        //$file_path=$file_sub_path.$file_name;   
-        //首先要判断给定的文件存在与否   
-        $file_path = 'public' . DS . 'luntan' . DS . 'down' .DS . redis-3.2.9.tar.gz‘
-        if(!file_exists($file_path)){  
-            echo "没有该文件文件";   
-            return ;   
-        }  
-        $fp=fopen($file_path,"r");  
-        $file_size=filesize($file_path);  
-        //下载文件需要用到的头  
-        Header("Content-type: application/octet-stream");  
-        Header("Accept-Ranges: bytes");   
-        Header("Accept-Length:".$file_size);   
-        Header("Content-Disposition: attachment; filename=".$file_path);   
+//        //用以解决中文不能显示出来的问题
+//        //$file_name=iconv("utf-8","gb2312",$file_name);
+//        //$file_sub_path=$_SERVER['DOCUMENT_ROOT']."marcofly/phpstudy/down/down/";
+//        //$file_path=$file_sub_path.$file_name;
+//        //首先要判断给定的文件存在与否
+//        $file_path = 'public' . DS . 'luntan' . DS . 'down' .DS . redis-3.2.9.tar.gz‘
+//        if(!file_exists($file_path)){
+//            echo "没有该文件文件";
+//            return ;
+//        }
+//        $fp=fopen($file_path,"r");
+//        $file_size=filesize($file_path);
+//        //下载文件需要用到的头
+//        Header("Content-type: application/octet-stream");
+//        Header("Accept-Ranges: bytes");
+//        Header("Accept-Length:".$file_size);
+//        Header("Content-Disposition: attachment; filename=".$file_path);
         $buffer=1024;   
         $file_count=0;   
         //向浏览器返回数据   
-        while(!feof($fp) && $file_count<$file_size){   
-            $file_con=fread($fp,$buffer);   
-            $file_count+=$buffer;   
-            echo $file_con;   
-        }  
-        fclose($fp);  
+//        while(!feof($fp) && $file_count<$file_size){
+//            $file_con=fread($fp,$buffer);
+//            $file_count+=$buffer;
+//            echo $file_con;
+//        }
+//        fclose($fp);
 	}
 }
