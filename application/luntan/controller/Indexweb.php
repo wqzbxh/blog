9678b9703240bd4efd3c1b5784bfdb9cd3ebcb6d
wanghaiyang <wqzbxh@163.com>
2018-05-12 21:13:07 +0800

    添加访问网站ip跟踪

--- application/luntan/controller/Indexweb.php
+++ application/luntan/controller/Indexweb.php
@@ -3,123 +3,126 @@ namespace app\luntan\controller;
 use think\Controller;
 
 /**
- * 
+ *
  * @author ZhuYong
  *
  *
  */
 class Indexweb extends Controller
 {
-		
-	//初始化函数
-	public function _initialize()
+
+    //初始化函数
+    public function _initialize()
     {
-      
+
     }
-	/**
-	*登录
-	*
-	*/ 
-	public function index()
-    {	
-		$limit = 10 ; 
-		$offset = 0;
-		$result = array();
-		$typeInfo = array();
-		$lunTanContentModel = new \app\admin\model\LuntanContent();
-		$lunTanTypeModel = new \app\admin\model\LuntanType();
-		$luntanLebalModel = new \app\admin\model\LuntanLebal();
-		$where = array('is_show'=>1,'is_del'=>0);
+    /**
+     *登录
+     *
+     */
+    public function index()
+    {
+
+        self::getVisitor();
+        $limit = 10 ;
+        $offset = 0;
+        $result = array();
+        $typeInfo = array();
+        $lunTanContentModel = new \app\admin\model\LuntanContent();
+        $lunTanTypeModel = new \app\admin\model\LuntanType();
+        $luntanLebalModel = new \app\admin\model\LuntanLebal();
+        $where = array('is_show'=>1,'is_del'=>0);
         $search = $_POST['search'] ?? null;
-        
-		$typeInfo = $lunTanTypeModel->getTpyes();
-		$lunTanContentRows = $lunTanContentModel->where($where);
-		if(empty($_POST['offset']) == false){
+
+        $typeInfo = $lunTanTypeModel->getTpyes();
+        $lunTanContentRows = $lunTanContentModel->where($where);
+        if(empty($_POST['offset']) == false){
             $offset = $_POST['offset'];
         }
         if(empty($_POST['limit']) == false){
             $limit = $_POST['limit'];
         }
 
-		$order = array('id' => 'desc');
-		$hotOrder = array('browse' => 'desc');
+        $order = array('id' => 'desc');
+        $hotOrder = array('browse' => 'desc');
 
-		if(!empty($search)){
+        if(!empty($search)){
             $lunTanContentRow = $lunTanContentRows->order($order)->where($lunTanContentModel->fuzzy_field,'like','%'.$search.'%')->limit($offset,$limit)->select();
         }else{
             $lunTanContentRow = $lunTanContentRows->order($order)->limit($offset,$limit)->select();
         }
 
-		//获取热门文章
-	   
-		$info = array();
-		if($lunTanContentRow){
-			foreach($lunTanContentRow as $contentRow){
-				$contentRow = $contentRow->toArray();
-				if($contentRow['type_id']){
-					$rows = $lunTanTypeModel->get(array('id'=>$contentRow['type_id']));
-					$row = $rows->toArray();
-					$result = $contentRow;
-					$result['typeTitle'] = $rows['content'];
-					
-				}	
-				if($contentRow['img_id']){
-				    $imageModel = new \app\admin\model\ImageRecord();
-				    $imgRow = $imageModel->get(array('id'=>$contentRow['img_id']));
-				    if($imgRow){
-				       $imgRow =  $imgRow->toArray();
-				       $result['path'] = $imgRow['path'];
-				       $info[] = $result;
-				    }
-				}
-			}
-		}
-		
-		$hotContent = $lunTanContentRows-> order($hotOrder)->limit(5)->select();
+        //获取热门文章
 
-		$hotInfo = array();
-		if($hotContent){
-		    foreach($hotContent as $hotRow){
-		        $hotRow = $hotRow->toArray();
-		        if(!empty($hotRow['type_id'])){
-		            $hotTypeRows = $lunTanTypeModel->get(array('id'=>$hotRow['type_id']));
+        $info = array();
+        if($lunTanContentRow){
+            foreach($lunTanContentRow as $contentRow){
+                $contentRow = $contentRow->toArray();
+                if($contentRow['type_id']){
+                    $rows = $lunTanTypeModel->get(array('id'=>$contentRow['type_id']));
+                    $row = $rows->toArray();
+                    $result = $contentRow;
+                    $result['typeTitle'] = $rows['content'];
 
-		            $hotTypeRows = $hotTypeRows->toArray();
-		            $hotResult = $hotRow;
-		            $hotResult['typeTitle'] = $hotTypeRows['content'];
-		            $hotInfo[] = $hotResult;
-		        }
+                }
+                if($contentRow['img_id']){
+                    $imageModel = new \app\admin\model\ImageRecord();
+                    $imgRow = $imageModel->get(array('id'=>$contentRow['img_id']));
+                    if($imgRow){
+                        $imgRow =  $imgRow->toArray();
+                        $result['path'] = $imgRow['path'];
+                        $info[] = $result;
+                    }
+                }
+            }
+        }
 
-		    }
-		}
-	   $luntanLebalModel = new \app\admin\model\LuntanLebal();
-	   $luntanLebalRows = $luntanLebalModel->where(array('is_show'=>1))->select();
-	   $lebalArray = array();
-	   if($luntanLebalRows){
-	       foreach ($luntanLebalRows as $LebalRows){
-	            $LebalRow = $LebalRows->toArray();
-	           $lebalArrays['name'] = $LebalRows['content'];
-               $lebalArrays['num'] = $lunTanContentModel->where($lunTanContentModel->label_query,'like','%'.$LebalRows['id'].'%')->where(array('is_del'=>0,'is_show'=>1))->count();
-               $lebalArray[] =  $lebalArrays;
-	       }
-	   }
-		$this->assign('lebalRow',$lebalArray);
-		$this->assign('contentRow',$info);
-		$this->assign('typeRow',$typeInfo);
-		$this->assign('hotInfo',$hotInfo);
+        $hotContent = $lunTanContentRows-> order($hotOrder)->limit(5)->select();
 
-		return $this->fetch('index');
+        $hotInfo = array();
+        if($hotContent){
+            foreach($hotContent as $hotRow){
+                $hotRow = $hotRow->toArray();
+                if(!empty($hotRow['type_id'])){
+                    $hotTypeRows = $lunTanTypeModel->get(array('id'=>$hotRow['type_id']));
+
+                    $hotTypeRows = $hotTypeRows->toArray();
+                    $hotResult = $hotRow;
+                    $hotResult['typeTitle'] = $hotTypeRows['content'];
+                    $hotInfo[] = $hotResult;
+                }
+
+            }
+        }
+        $luntanLebalModel = new \app\admin\model\LuntanLebal();
+        $luntanLebalRows = $luntanLebalModel->where(array('is_show'=>1))->select();
+        $lebalArray = array();
+        if($luntanLebalRows){
+            foreach ($luntanLebalRows as $LebalRows){
+                $LebalRow = $LebalRows->toArray();
+                $lebalArrays['name'] = $LebalRows['content'];
+                $lebalArrays['num'] = $lunTanContentModel->where($lunTanContentModel->label_query,'like','%'.$LebalRows['id'].'%')->where(array('is_del'=>0,'is_show'=>1))->count();
+                $lebalArray[] =  $lebalArrays;
+            }
+        }
+        $this->assign('lebalRow',$lebalArray);
+        $this->assign('contentRow',$info);
+        $this->assign('typeRow',$typeInfo);
+        $this->assign('hotInfo',$hotInfo);
+
+        return $this->fetch('index');
     }
-    
-  /**
-   * 
-   * @return mixed
-   */
-    
-    
-	public function arctic()
-    {	
-        $arcticRow = array();        
+
+    /**
+     *
+     * @return mixed
+     */
+
+
+    public function arctic()
+    {
+        self::getVisitor();
+        $arcticRow = array();
         $returnArray = array();
         $labelType = array();
         if(!empty($_GET)){
@@ -127,9 +130,9 @@ class Indexweb extends Controller
                 $id = $_GET['id'];
             }else{
                 $returnArray = array(
-                      'code' => 100002,
-                      'msg'  =>  '参数错误',
-                      'data' => array()
+                    'code' => 100002,
+                    'msg'  =>  '参数错误',
+                    'data' => array()
                 );
             }
         }else{
@@ -139,17 +142,17 @@ class Indexweb extends Controller
                 'data' => array()
             );
         }
-        
+
         $ImageRecordModel = new \app\admin\model\ImageRecord();
         $lunTanContentModel = new \app\admin\model\LuntanContent();
         $lunTanTypeModel = new \app\admin\model\LuntanType();
         $labelModel = new \app\admin\model\LuntanLebal();
-        
+
         $where = array('is_show'=>1,'is_del'=>0);
         //获取分类：
         $contentRow = $lunTanContentModel->get(array('id'=>$id,'is_show'=>1,'is_del'=>0))->toArray();
-        
-        
+
+
         if($contentRow['type_id']){
             $rows = $lunTanTypeModel->get(array('id'=>$contentRow['type_id']));
             $row = $rows->toArray();
@@ -172,36 +175,36 @@ class Indexweb extends Controller
             }
         }
         if($contentRow['img_id']){
-			$ImageRecordRows = $ImageRecordModel->get(array('id'=>$contentRow['img_id']));
-			$ImageRecordRow = $ImageRecordRows->toArray();
-			$arcticRow["img_oss"] = $ImageRecordRow['path'];
-		}
+            $ImageRecordRows = $ImageRecordModel->get(array('id'=>$contentRow['img_id']));
+            $ImageRecordRow = $ImageRecordRows->toArray();
+            $arcticRow["img_oss"] = $ImageRecordRow['path'];
+        }
         $arcticRow['labelType'] =$labelType;
         $typeTitle = $lunTanTypeModel->where($where)->field('content')->select();
         $typeInfo = array();
         foreach ($typeTitle as $typeRow){
             $typeRow = $typeRow->toArray();
             $typeInfo[] =$typeRow['content'];
-        }	
+        }
 
         $this->assign('contentRow',$arcticRow);
-		$this->assign('typeRow',$typeInfo);
+        $this->assign('typeRow',$typeInfo);
 // 		$this->assign('hotInfo',$hotInfo);
-		return $this->fetch('arctic');
-    }		
-	
-	/**
-	*位置显示
-	*
-	*/
-	public function mylocaltion(){
-		return $this->fetch('mylocaltion');
-	}
-	
-	/**
-	*测试
-	*
-// 	*/
+        return $this->fetch('arctic');
+    }
+
+    /**
+     *位置显示
+     *
+     */
+    public function mylocaltion(){
+        return $this->fetch('mylocaltion');
+    }
+
+    /**
+     *测试
+     *
+    // 	*/
 // 	public function test($i){
 // 		header("Content-Type:text/html;charset=UTF-8");
 // 		date_default_timezone_set("PRC");
@@ -218,7 +221,7 @@ class Indexweb extends Controller
 
 // 			//创建参数(包括签名的处理)
 // 			function createParam ($paramArr,$showapi_secret) {
-				
+
 // 			$paraStr = "";
 // 			$signStr = "";
 // 			ksort($paramArr);
@@ -254,37 +257,67 @@ class Indexweb extends Controller
 // 					return;
 // 				}
 // 			}
-																					
-// 	}
-	
-	public function testaction(){
-		
-        $lunTanContentModel = new \app\admin\model\LuntanContent();
-	    $redis = $lunTanContentModel->iniRedis();
-	    $info = array(
-	        'code' => 1, 
-	        'msg' => 'SUCCESS',
-	        'data' => array(
-	               'name' => 'haiyang',
-	               'age' => '22',
-	               'sex' => '男',
-	               'phone' => '13512169551',
-	        )
-	    );
-	    
-	    $result = json_encode($info);
-	    
-		$a = $redis->set('myname',$result); 
-		
-	}
-	public function test(){
-        $ip = $_SERVER["REMOTE_ADDR"];
-        $lunTanContentModel = new \app\admin\model\LuntanContent();
-	    $redis = $lunTanContentModel->pvRedis();
 
-	    $luntanModel = new \app\admin\model\LuntanType();
-	    $row = $luntanModel->getTpye();
-	    var_dump($row);
-	    
-	}
+// 	}
+
+    public function testaction(){
+
+        $lunTanContentModel = new \app\admin\model\LuntanContent();
+        $redis = $lunTanContentModel->iniRedis();
+        $info = array(
+            'code' => 1,
+            'msg' => 'SUCCESS',
+            'data' => array(
+                'name' => 'haiyang',
+                'age' => '22',
+                'sex' => '男',
+                'phone' => '13512169551',
+            )
+        );
+
+        $result = json_encode($info);
+
+        $a = $redis->set('myname',$result);
+
+    }
+    public function getVisitor(){
+        $ip = $_SERVER["REMOTE_ADDR"];
+        $agent=$_SERVER["HTTP_USER_AGENT"];
+        $operateData = file_get_contents('http://ip.taobao.com/service/getIpInfo.php?ip='.$ip);
+        $operate = json_decode($operateData,true);
+        $oper = $operate['data'];
+        $page = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
+        $lunTanContentModel = new \app\admin\model\LuntanContent();
+        $redis = $lunTanContentModel->pvRedis();
+        $datetime =  date('Y-m-d H:i:s');
+        if($redis){
+            $ipResult = $redis->rpush('IP',$ip.'WQZB'.$datetime.'WQZB'.$page.'WQZB'.$oper['country'].'WQZB'.$oper['city'].'WQZB'.$oper['isp'].'WQZB'.$agent);
+        }
+    }
+
+    public function setVisitor()
+    {
+        $lunTanContentModel = new \app\admin\model\LuntanContent();
+        $VisitorModel = new \app\admin\model\LuntanVisitor();
+        $redis = $lunTanContentModel->pvRedis();
+        // $labelRows = explode(',', $contentRow['label_id']);
+        if($redis){
+            for($i = 0; $i < 500;  $i++  ){
+                $reultRows = $redis->lpop('IP');
+                $result = explode('WQZB',$reultRows);
+                if(!empty($result[0])){
+                    $data['host'] = $result[0] ?? '未知';
+                    $data['create_at'] = $result[1] ?? '2018-05-12 17:08:28';
+                    $data['page'] = $result[2] ?? '未知';
+                    $data['country'] = $result[3] ?? '未知';
+                    $data['city'] = $result[4] ?? '未知';
+                    $data['isp'] = $result[5] ?? '未知';
+                    $data['browser'] = $result[6] ?? '未知';
+                    $VisitorModel->create($data);
+                }else{
+                    return ;
+                }
+            }
+        }
+    }
 } 
